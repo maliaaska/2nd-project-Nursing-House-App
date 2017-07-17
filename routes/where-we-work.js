2a14b1ca71b73f5aec931004d63ceb67292034e6
Arkadiusz Szymon Milewski <asmilewski@gmail.com>
2017-07-17 10:27:52 +0200

    trial to merg with Petra's repository

--- routes/where-we-work.js
+++ routes/where-we-work.js
@@ -4,10 +4,8 @@ var auth    = require('../helpers/auth');
 
 
 router.get('/where-we-work', function(req, res, next) {
-  res.render('where-we-work', { title: 'Find a place where you can help!' });
+  res.render('map');
 });
-
-
 
 
 module.exports = router;
